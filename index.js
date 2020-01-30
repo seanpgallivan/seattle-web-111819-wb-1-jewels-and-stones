b42bbcd03326df35dcb0df5d40a10f52739f9928
MatthewBrophy <m.broph@gmail.com>
2020-01-29 17:16:09 -0800

    1

---
+++ index.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("hey look, some code!");
+  jewelsAndStones();
+});
+
+jewelsAndStones = (jewels, stones) => {
+  console.log("checking which stones are jewels...");
+  //code your answer here!!!
+};
